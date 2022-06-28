5038635415e8dac5ac9c0d9f3595898db2ba3b2f
Alberto Duarte <betodute@gmail.com>
2022-06-28 11:23:44 -0700

    Styling
    It is possible to also add styling as the element is created!

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const element = React.createElement(
   'h1',
   {
     className: 'title',
-    onClick: () => {console.log('clicked')}
+    onClick: () => {console.log('clicked')},
+    style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
   },
   'Hello World!'
 );
