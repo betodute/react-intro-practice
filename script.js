2277554a60d9947a629cdf81851a19f22f0f96fb
Alberto Duarte <betodute@gmail.com>
2022-06-30 00:08:53 -0700

    Added Spans to HTML
    Added span elements to html now need to extract the id from each and deposit it as the child of each span to display it on the dom

--- script.js
+++ script.js
@@ -4,12 +4,42 @@ const element = React.createElement(
   {
     className: 'title',
     onClick: () => {console.log('clicked')},
-    style: { fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
+    style: {fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
   },
   'Hello World!'
 );
+
+const element2 = React.createElement(
+  'h2',
+  {
+    className: 'title',
+    onClick: () => {console.log('clicked 2')},
+    style: {fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'}
+  },
+  'Hello World 2'
+)
+
+const element3 = React.createElement(
+  'h3',
+  {
+    className: 'title',
+    onClick: () => {console.log('clicked3')},
+    style: {fontFamily: 'sans-serif', color: 'rgb(255, 100, 100)'},
+  },
+  'Hello World 3'
+)
 
 ReactDOM.render(
   element,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  element2,
+  document.getElementById('root2')
+);
+
+ReactDOM.render(
+  element3,
+  document.getElementById('root3')
+)
